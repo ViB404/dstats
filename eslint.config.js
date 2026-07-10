c3b583fd367426707caeec5b6a05732f44c41fa6
ViB404 <justvibrio@gmail.com>
2026-07-10 15:15:37 +0530

    fix(eslint): include jsx and tsx files in config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,12 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactCompiler from "eslint-plugin-react-compiler";
 
-export default defineConfig({
-	files: ["**/*.{js,jsx,ts,tsx}"],
-	extends: [js.configs.recommended, tseslint.configs.recommended, reactCompiler.configs.recommended],
-});
+export default defineConfig(
+	{
+		ignores: ["**/node_modules/", ".git/", "**/dist/", "**/.next/"],
+	},
+	{
+		files: ["**/*.{js,jsx,ts,tsx}"],
+		extends: [js.configs.recommended, tseslint.configs.recommended, reactCompiler.configs.recommended],
+	}
+);
